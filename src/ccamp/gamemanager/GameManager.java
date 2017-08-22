7ecb7532f3c1b087a419939ed3f2a16c84d25504
D0sto dost0 <diefischmail@gmail.com>
2017-08-22 12:38:42 +0200

    Addes vars and lists

--- src/ccamp/gamemanager/GameManager.java
+++ src/ccamp/gamemanager/GameManager.java
@@ -1,9 +1,25 @@
 package ccamp.gamemanager;
 
+import ccamp.Ball;
 import ccamp.Player.Player;
+import ccamp.blocks.Blocks;
+
+import java.util.List;
 
 public class GameManager {
     Player player;
+    List<Ball> ballList;
+    List<Blocks> blockList;
+
+    int score;
+    int maxPlayerSpeed;
+    int minPlayerSpeed;
+    int maxPlayerWidth;
+    int minPlayerWidth;
+
+    public void addBall(Ball ball){
+        //Add a ball
+    }
 
     public void decrementLife() {
 
@@ -12,5 +28,14 @@ public class GameManager {
 
     public void constructGame() {
         //If item was hit
+    }
+
+    public boolean isGameOver() {
+        if (player.getLives() <= 0){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }

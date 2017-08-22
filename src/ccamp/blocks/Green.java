7318860b6d4e1cf5e29b72855a8ff16b94d248e7
Henry Pawlak <henrpawl17@computercamp.local>
2017-08-22 12:40:09 +0200

    Add BlockType

--- src/ccamp/blocks/Green.java
+++ src/ccamp/blocks/Green.java
@@ -6,8 +6,8 @@ public class Green extends Block{
 
     private int speedLower;
 
-    public green(int posX, int posY, int HP, Color color, int speedLower) {
-        super(posX, posY, HP, color);
+    public green(int posX, int posY, int HP, Color color, int speedLower, int width, int height, BlockType type) {
+        super(posX, posY, HP, color, width, height, type);
         this.speedLower = speedLower;
     }
 

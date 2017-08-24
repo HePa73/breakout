000c8c49062b6d756ff0f32d2161569f5ae1d11c
Philip Vanderspruit <philvand17@computercamp.local>
2017-08-24 17:33:26 +0200

    worked on renderinfos

--- src/ccamp/render/renderinfo/RawRenderInfo.java
+++ src/ccamp/render/renderinfo/RawRenderInfo.java
@@ -1,5 +1,12 @@
 package ccamp.render.renderinfo;
 
+import javafx.scene.paint.Color;
+
 public class RawRenderInfo extends RenderInfo {
 
+    private Color color;
+
+    public RawRenderInfo(int x, int y) {
+        super(x, y);
+    }
 }
--- src/ccamp/render/renderinfo/TextRenderInfo.java
+++ src/ccamp/render/renderinfo/TextRenderInfo.java
@@ -1,5 +1,16 @@
 package ccamp.render.renderinfo;
 
+import sun.font.FontFamily;
+
+import javax.swing.text.StyledEditorKit;
+
 public class TextRenderInfo extends RenderInfo {
 
+    private int fontSize;
+    private FontFamily fontFamily;
+
+    public TextRenderInfo(int x, int y) {
+        super(x, y);
+
+    }
 }
